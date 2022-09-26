8145aeb3db1773fee3349444d3c7ca8535f9d2e7
MarcosRos4 <103190249+MarcosRos4@users.noreply.github.com>
2022-09-26 19:17:42 -0300

    ivoholanda

---
+++ src/peides/Rome/index.js
@@ -0,0 +1,14 @@
+import trans from "./transferir.png"
+
+function Rome(){
+    return(
+        <div>
+            <h1>TELA DO ROME</h1>
+            <h3>TOP TEXT</h3>
+            <img src={trans}/>
+            <h2>BOTTOM TEXT</h2>
+        
+        </div>
+    )
+}
+export default Rome;
---
+++ src/peides/Sobre/index.js
@@ -0,0 +1,14 @@
+import sob from "./sobre.png"
+
+function Sobre(){
+    return(
+        <div>
+            <h1>TELA DO SORBENOS</h1>
+            <h3>TOP TEXT</h3>
+            <img src={sob}/>
+            <h2>BOTTOM TEXT</h2>
+        
+        </div>
+    )
+}
+export default Sobre;
